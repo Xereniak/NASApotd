68888186cafad3935b21f0df3c47425e5d2cb08b
West <west2@uleth.ca>
2019-02-05 13:05:43 -0700

    First Commit

--- www/js/main.js
+++ www/js/main.js
@@ -2,18 +2,18 @@ var app = new Framework7({
     //    INITIALIZE APP
     root: '#app',
 
-    //    PANEL RULES
-    panel: {
-        swipe: 'both',
-        swipeActiveArea: 50,
-    },
-    //    PAGE RULES
-    routes: [
-        {
-            path: '/page2/',
-            url: 'pages/page2.html'
-        }
-    ],
+    //    //    PANEL RULES
+    //    panel: {
+    //        swipe: 'both',
+    //        swipeActiveArea: 50,
+    //    },
+    //    //    PAGE RULES
+    //    routes: [
+    //        {
+    //            path: '/page2/',
+    //            url: 'pages/page2.html'
+    //        }
+    //    ],
 });
 
 var mainView = app.views.create('.view-main');
